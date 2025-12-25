b8331332b2b2d66a474a0c24fbbaf25c9df60a69
seunbantu <oogunsanwo@sb-synergy.com>
2025-12-25 11:43:53 -0500

    Create flight_search.py

---
+++ flight_search.py
@@ -0,0 +1,12 @@
+def search_flight(origin, destination):
+    """
+    Simulated flight search.
+    Replace this with real API logic in advanced lessons.
+    """
+    return {
+        "origin": origin,
+        "destination": destination,
+        "price": 420,
+        "date": "2026-03-12",
+        "airline": "Delta"
+    }
